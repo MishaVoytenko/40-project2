5e30b906d65343b61d2c042da9f6fed04a28b2a5
MishaVoytenko <minyavoytenko@gmail.com>
2023-02-06 23:19:59 +0200

    add slider

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).on('ready', function () {
+  $('.header-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: false,
+  });
+})
+
+
+
