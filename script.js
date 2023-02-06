e4c019ce44976eb28838d4d417c19e5bebc42342
MishaVoytenko <minyavoytenko@gmail.com>
2023-02-07 00:33:06 +0200

    commit

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(document).on('ready', function () {
   $('.header-slider').slick({
     dots: true,
     infinite: true,
-    speed: 300,
+    speed: 1000,
     slidesToShow: 1,
     adaptiveHeight: true,
     arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
   });
 })
 
